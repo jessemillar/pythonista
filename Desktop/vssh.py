6e27f5c491dc491ff2c53ff50bc3e978d9e664b7
Jesse Millar <hellojessemillar@gmail.com>
2016-02-26 18:37:43 -0700

    Fixing some bugs that messed up connecting to Virtualbox machines

--- Desktop/vssh.py
+++ Desktop/vssh.py
@@ -1,4 +1,5 @@
-"""Find the IP address of a VirtualBox virtual machine and ssh into it. Add or update /etc/hosts entries upon user interaction."""
+"""Find the IP address of a VirtualBox virtual machine and ssh into it.\
+   Add or update /etc/hosts entries upon user interaction."""
 
 import os
 import subprocess
@@ -8,26 +9,29 @@ import sys
 def check_exists(name):
     """Check if the virtual machine exists."""
     virtualbox_exists = subprocess.Popen(["VBoxManage", "list", "vms"], stdout=subprocess.PIPE)
-    grep_exists = subprocess.Popen(["grep", "-c", name], stdin=virtualbox_exists.stdout, stdout=subprocess.PIPE)  # Returns a string ("0", "1") stating whether the virtual machine is running; I cast the string to an int later on
 
-    return bool(grep_exists.communicate()[0])
+    if name in virtualbox_exists.communicate()[0]:
+        return True
+    else:
+        return False
 
 
 def check_up(name):
     """Check if the virtual machine is currently powered on."""
     virtualbox_up = subprocess.Popen(["VBoxManage", "list", "runningvms"], stdout=subprocess.PIPE)
-    grep_up = subprocess.Popen(["grep", "-c", name], stdin=virtualbox_up.stdout, stdout=subprocess.PIPE)
 
-    return bool(grep_up.communicate()[0])
+    if name in virtualbox_up.communicate()[0]:
+        return True
+    else:
+        return False
 
 
 def find_host(name):
-    """Check if an entry for the virtual machine already exists in /etc/hosts."""
+    """Check if an entry already exists in /etc/hosts."""
     hosts = open("/etc/hosts", "r")
     for line in hosts:
-        if len(line.split()) > 1:
-            if " ".join(line.split()[1:]) == name:
-                return True
+        if name in line:
+            return True
 
     return False
 
@@ -36,8 +40,8 @@ def host_outdated(address, name):
     """Check if the entry for the virtual machine in /etc/hosts is outdated."""
     hosts = open("/etc/hosts", "r")
     for line in hosts:
-        if len(line.split()) > 1:
-            if line.split()[0] != address and " ".join(line.split()[1:]) == name:
+        if name in line:
+            if address not in line:
                 return True
 
     return False
@@ -50,23 +54,29 @@ def add_host(address, name):
     temp_hosts = open("/tmp/etc_hosts.tmp", "wt")
     temp_hosts.write(hosts_contents)
 
-    os.system("sudo mv /tmp/etc_hosts.tmp /etc/hosts")  # Move the temp hosts file into place with sudo permissions
+    # Move the temp hosts file into place with sudo permissions
+    os.system("sudo mv /tmp/etc_hosts.tmp /etc/hosts")
 
 
 def update_host(address, name):
     """Update an entry in /etc/hosts to have the correct IP address."""
     hosts = open("/etc/hosts", "r")
     data = hosts.readlines()
+    new_hosts = []
 
-    for i in range(0, len(data)):
-        if len(data[i].split()) > 1:
-            if " ".join(data[i].split()[1:]) == name:
-                data[i] = address + "\t" + name + "\n"
+    for line in data:
+        if name in line:
+            new_hosts.append(address + "\t" + name + "\n")
+        else:
+            new_hosts.append(line)
+
+    print new_hosts
 
     temp_hosts = open("/tmp/etc_hosts.tmp", "wt")
-    temp_hosts.writelines(data)
+    temp_hosts.writelines(new_hosts)
 
-    os.system("sudo mv /tmp/etc_hosts.tmp /etc/hosts")  # Move the temp hosts file into place with sudo permissions
+    # Move the temp hosts file into place with sudo permissions
+    os.system("sudo mv /tmp/etc_hosts.tmp /etc/hosts")
 
 
 def main():  # Define as a function to adhere to style guidelines
@@ -77,9 +87,11 @@ def main():  # Define as a function to adhere to style guidelines
         print "Missing name of virtual machine"
         return
 
+    # Check if the user is supplying the virtual machine's name correctly
     try:
-        sys.argv[2]  # Check if the user is supplying the virtual machine's name correctly
-    except IndexError:  # If the name is correct, run the program
+        sys.argv[2]
+    # If the name is correct, run the program
+    except IndexError:
         if not check_exists(sys.argv[1]):
             print "The specified virtual machine does not appear to exist."
             return
@@ -94,26 +106,25 @@ def main():  # Define as a function to adhere to style guidelines
             else:
                 return
 
-        virtualbox_ip_cmd = [
-            "vboxmanage", "guestproperty", "get", sys.argv[1], "/VirtualBox/GuestInfo/Net/0/V4/IP"
-        ]
-
-        virtualbox_ip = subprocess.Popen(virtualbox_ip_cmd, stdout=subprocess.PIPE)
-        address = virtualbox_ip.communicate()[0].split()[1]
+        virtualbox_ip = subprocess.Popen(["VBoxManage", "guestproperty", "get", sys.argv[1], "/VirtualBox/GuestInfo/Net/0/V4/IP"], stdout=subprocess.PIPE)
+        ip_response = virtualbox_ip.communicate()[0]
+        if ip_response == "No value set!\n":
+            print "Could not find the virtual machine's IP address. Are network settings configured correctly?"
+            return
 
         if find_host(sys.argv[1]):
-            if host_outdated(address, sys.argv[1]):
+            if host_outdated(ip_response.split()[1], sys.argv[1]):
                 hosts_input = raw_input("/etc/hosts has an outdated entry for this virtual machine. Would you like to update it? [Y/n] ")
 
                 if len(hosts_input) == 0 or hosts_input == "Y" or hosts_input == "y":  # If the user responds in the affirmative
-                    update_host(address, sys.argv[1])
+                    update_host(ip_response.split()[1], sys.argv[1])
         else:
             hosts_input = raw_input("/etc/hosts does not have an entry for this virtual machine. Would you like to add one? [Y/n] ")
 
             if len(hosts_input) == 0 or hosts_input == "Y" or hosts_input == "y":  # If the user responds in the affirmative
-                add_host(address, sys.argv[1])
+                add_host(ip_response.split()[1], sys.argv[1])
 
-        os.system("ssh " + address)
+        os.system("ssh " + ip_response.split()[1])
     else:
         print "If your virtual machine's name contains spaces, please wrap it in quotes."
         return
